76b83de00113b59e8675d33545a9156b0af3abb6
hooliooo <julio.alorro@ffuf.de>
2022-02-01 13:37:37 +0100

    Initial Commit

---
+++ AVPlayerExercise/AVPlayerExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVPlayerExerciseApp.swift
+//  AVPlayerExercise
+//
+//  Created by Julio Alorro on 01.02.22.
+//
+
+import SwiftUI
+
+@main
+struct AVPlayerExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVPlayerExercise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AVPlayerExercise
+//
+//  Created by Julio Alorro on 01.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
